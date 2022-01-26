f66a325f53b3257cc3f66702a9fd75a91334f370
duanfeiyang <duanfeiyang@lattebank.com>
2022-01-26 15:59:22 +0800

    层序遍历

--- src/main/java/二叉树/a/二叉树的层序遍历.java
+++ src/main/java/二叉树/a/二叉树的层序遍历.java
@@ -1,12 +1,13 @@
 package 二叉树.a;
 
+import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.List;
 
 /**
- *  给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
- *  输入：root = [3,9,20,null,null,15,7]
- *  输出：[[3],[9,20],[15,7]]
+ * 给你二叉树的根节点 root ，返回其节点值的 层序遍历 。 （即逐层地，从左到右访问所有节点）。
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：[[3],[9,20],[15,7]]
  */
 public class 二叉树的层序遍历 {
 
@@ -21,25 +22,44 @@ public class 二叉树的层序遍历 {
         // 前序遍历
         rootPrint(root);
         // 层序遍历
-
+        List<List<Integer>> lists = levelOrder(root);
+        System.out.println();
     }
 
-    static void rootPrint(TreeNode root){
+    static void rootPrint(TreeNode root) {
         System.out.print(root.val);
-        if(root.left != null ){
+        if (root.left != null) {
             rootPrint(root.left);
         }
-        if(root.right != null ){
+        if (root.right != null) {
             rootPrint(root.right);
         }
     }
 
     public static List<List<Integer>> levelOrder(TreeNode root) {
         ArrayList<List<Integer>> result = new ArrayList<>();
-        List<Integer> level = new ArrayList<>();
-        return null;
+        ArrayDeque<TreeNode> queue = new ArrayDeque<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            int num = queue.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < num; i++) {
+                TreeNode node = queue.poll();
+                if (node == null) {
+                    break;
+                }
+                level.add(node.val);
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+            result.add(level);
+        }
+        return result;
     }
-
 
 
 }
